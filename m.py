6e9cd12c0bebca0139e9833c2233434408b3d9a7
og-ryuk001 <a04498438@gmail.com>
2024-07-17 12:13:06 +0530

    Update m.py

--- m.py
+++ m.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7236712231:AAG9D9N2Iv7zC7oPrT2rbP0y8FcK-IpudYM'
+TOKEN = '7283418311:AAG8JirJWjMQpCcpyF_MtPP6pw7ouGDTiLg'
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -4211947505
-CHANNEL_ID = -4211947505
-error_channel_id = -4211947505
+FORWARD_CHANNEL_ID = -4268392392
+CHANNEL_ID = -4268392392
+error_channel_id = -4268392392
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
